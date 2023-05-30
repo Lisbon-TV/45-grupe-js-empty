4c0d7f05b8749d1dcdb05485e1b82522407dc2d9
Lisbon <lisbon@dublin.com>
2023-05-30 10:56:48 +0300

    Numbers

---
+++ js/02-numbers.js
@@ -0,0 +1,17 @@
+console.clear();
+
+// skaiciai
+
+console.log(Infinity);
+console.log(Nan);
+
+console.log(2 + 2);
+
+
+// aritmeniniai operatoriai
+
+console.log(clear);
+
+console.log(2);
+
+const pinigine = 0;
